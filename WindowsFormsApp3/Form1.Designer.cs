e2662c7b7258bdb015d54237856e08df910963a0
lesiu199 <32600500+lesiu199@users.noreply.github.com>
2017-10-20 16:53:39 +0200

    Initial commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DragandDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lb_MouseDown);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.lb_DragEnter);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.lb_DragDrop);
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(213, 37);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(170, 199);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lb_MouseDown);
+            this.listBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.lb_DragEnter);
+            this.listBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.lb_DragDrop);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 267);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(437, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 289);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void ListBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragandDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("hahah");
+            listBox1.Items.Add("ddddd");
+            listBox2.Items.Add("2222");
+            listBox2.Items.Add("grge");
+            listBox2.Items.Add("1111");
+        }
+       
+        private void lb_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragDropEffects effects =
+                (sender as ListBox).DoDragDrop((sender as ListBox).SelectedItem, DragDropEffects.Move);
+
+            if (effects != DragDropEffects.None)
+            {
+                (sender as ListBox).Items.RemoveAt((sender as ListBox).SelectedIndex);
+            }
+        }
+
+
+
+        private void lb_DragEnter(object sender, DragEventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Przesyłam dane...";
+
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void lb_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                Object item = (object)e.Data.GetData(typeof(System.String));
+
+                (sender as ListBox).Items.Add(item);
+                toolStripStatusLabel1.Text = "";
+            }
+        }
+
+    }
+}
